9567bdfce043802c87f022a2b68872cdc06134e8
inq968 <51177187+inq968@users.noreply.github.com>
2021-04-27 01:58:49 -0500

    Update and rename FXMLLoader.java to FxmlLoader.java
    Added a comment and changed "package sample" to "package application"

--- FxmlLoader.java
+++ FxmlLoader.java
@@ -1,10 +1,14 @@
-package sample;
+package application;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
 
+/* This class has one method, getPage, with a String parameter containing
+ * the file name. It is intended to reduce the lines of code needed for
+ * loading an FXML file.
+ */
 public class FxmlLoader {
     private Pane view;
 
