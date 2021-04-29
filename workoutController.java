ee16b3691a13b7aed99380a54f5995658194f248
boaxiaodi <80645799+boaxiaodi@users.noreply.github.com>
2021-04-29 00:04:36 -0500

    Create workoutController.java

---
+++ workoutController.java
@@ -0,0 +1,39 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+public class workoutController {
+
+    @FXML
+    private AnchorPane workoutbg;
+    @FXML
+    private BorderPane trackpane2;
+    @FXML
+    private TextField workoutInput;
+    @FXML
+    private TextField reps;
+    @FXML
+    private TextField sets;
+    @FXML
+    private Button addWorkoutBtn;
+    @FXML
+    private Button removeWorkoutBtn;
+    @FXML
+    private Button backbtn;
+
+
+//    @FXML
+//    void addWorkout(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void removeWorkout(ActionEvent event) {
+//
+//    }
+
+}
