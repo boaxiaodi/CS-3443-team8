ae2875c65c7769c547ee2ee7a1f5855aec4e7a2a
Angelo M <80708054+tjn559@users.noreply.github.com>
2021-04-29 20:38:15 -0500

    Update workoutController.java

--- workoutController.java
+++ workoutController.java
@@ -1,10 +1,16 @@
 package application;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import model.Model;
+import javafx.scene.layout.Pane;
 
 public class workoutController {
 
@@ -15,25 +21,40 @@ public class workoutController {
     @FXML
     private TextField workoutInput;
     @FXML
-    private TextField reps;
+    private TextField workoutReps;
     @FXML
-    private TextField sets;
+    private TextField workoutSets;
     @FXML
     private Button addWorkoutBtn;
     @FXML
     private Button removeWorkoutBtn;
     @FXML
     private Button backbtn;
+    @FXML
+    private ListView<String> linkList;
+    @FXML
+    private ListView<String> workoutList;
 
 
-//    @FXML
-//    void addWorkout(ActionEvent event) {
-//
-//    }
-//
-//    @FXML
-//    void removeWorkout(ActionEvent event) {
-//
-//    }
+    /* This method clears the ListViews and the TextFields.
+     */
+    private void clear() {
+        //ObservableList<String> cl = FXCollections.observableArrayList("");
+        workoutInput.setText("");
+        workoutReps.setText("");
+        workoutSets.setText("");
+        linkList.setItems(FXCollections.observableArrayList(""));
+    }
 
+    @FXML
+    public void addWorkout(ActionEvent actionEvent) throws Exception{
+        String exercise = workoutInput.getText();
+        String reps = workoutReps.getText();
+        String sets = workoutSets.getText();
+        Model.addWorkout(exercise, reps, sets);
+        clear();
+    }
+
+    public void removeWorkout(ActionEvent actionEvent) {
+    }
 }
